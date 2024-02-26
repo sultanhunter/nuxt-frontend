17b9c3dc89464b5e997a9eba76f552424ecae8d9
sultan <sultanibneusman0@gmail.com>
2024-02-26 12:23:24 +0530

    Revert "refactor"
    This reverts commit e03b9e31169cc43cbd9c296115d222b1c7442dd4.

--- composables/useCapture.ts
+++ composables/useCapture.ts
@@ -48,7 +48,7 @@ export default () => {
 
     const result = () => useState<Result | null>('result', () => null)
 
-    const isFetchingData = useState('isLoading', () => false)
+    const isFetchingData = useState('isLoading', () => true)
 
     const isSubmittingData = useState('isSubmitting', () => false)
 
@@ -58,17 +58,15 @@ export default () => {
         // const url = 'http://localhost:8000/capture'
         try {
 
-            let timerCompleted = false
-
-            setTimeout(() => {
-                console.log('timer body')
-                const data = cityVehicleData().value
-                if (data.cities.length > 0 && data.vehicles.length > 0) {
-                    console.log('fetching complete')
-                    isFetchingData.value = false
-                }
-                timerCompleted = true
-            }, 1500)
+            // let timerCompleted = false
+            //
+            // setTimeout(() => {
+            //     const data = cityVehicleData().value
+            //     if (data.cities.length > 0 && data.vehicles.length > 0) {
+            //         isFetchingData.value = false
+            //     }
+            //     timerCompleted = true
+            // }, 1500)
 
             const {data} = await axios.get(url)
 
@@ -77,10 +75,10 @@ export default () => {
             const ourData = cityVehicleData()
             ourData.value = cityVehicleDataFromApi
 
-            if (timerCompleted) {
-                console.log('Got results after timer completed')
-                isFetchingData.value = false
-            }
+            // if (timerCompleted) {
+            //     isFetchingData.value = false
+            // }
+            isFetchingData.value = false
 
         } catch (e) {
             isFetchingData.value = false
@@ -93,8 +91,8 @@ export default () => {
             alert('Select all options')
             return
         }
-        // const url = 'https://nuxt-backend-vez7.onrender.com/capture'
-        const url = 'http://localhost:8000/capture'
+        const url = 'https://nuxt-backend-vez7.onrender.com/capture'
+        // const url = 'http://localhost:8000/capture'
         const body = {
             'copChoices': selectionState().value.map((choice) => ({
                 'cop': choice.copIndex,
